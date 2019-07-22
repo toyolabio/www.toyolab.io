0aaf78ada817dc640d78a40f29e8f6702d822784
kazuhisa <kazu87366@outlook.jp>
2019-07-22 16:47:37 +0900

    #10 delete contacts from footer and its page

--- src/components/footer.js
+++ src/components/footer.js
@@ -39,6 +39,17 @@ const Column = ({ title, links }) => (
   </Col>
 )
 
+const Column2 = ({ title }) => (
+  <Col span={24} sm={12} md={8} style={{ marginBottom: "2rem" }}>
+    <Title style={{ color: "lightgray" }} level={3}>
+      {title}
+    </Title>
+    <a href="mailto:mail@mail.com?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B&amp;body=%E4%B8%8B%E8%A8%98%E3%81%AE%E9%A0%85%E7%9B%AE%E3%81%AB%E3%81%9D%E3%82%8C%E3%81%9E%E3%82%8C%E8%A8%98%E5%85%A5%E3%81%97%E3%81%A6%E9%80%81%E4%BF%A1%E3%81%97%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84%E3%80%82%0D%0A%0D%0A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%0D%0A%E3%81%8A%E5%90%8D%E5%89%8D%EF%BC%9A%E3%80%8C%E3%80%8D%0D%0A%E9%9B%BB%E8%A9%B1%E7%95%AA%E5%8F%B7%EF%BC%9A%E3%80%8C%E3%80%8D%0D%0A%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B%E5%86%85%E5%AE%B9%EF%BC%9A%E3%80%8C%E3%80%8D%0D%0A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A%2A">
+      mail@mail.com
+    </a>
+  </Col>
+)
+
 export default () => (
   <ModifiedFooter>
     <FooterWrapper>
@@ -50,7 +61,6 @@ export default () => (
             { title: "Annoucements", path: `/announcements`, icon: "bell" },
             { title: "Blog", path: `/blog`, icon: "read" },
             { title: "Members", path: `/members`, icon: "team" },
-            { title: "Contact", path: `/contact`, icon: "mail" },
           ]}
         />
         <Column
@@ -65,6 +75,7 @@ export default () => (
           title="Products"
           links={[{ title: "Events", path: `/events`, icon: "calendar" }]}
         />
+        <Column2 title="Contact" />
         <Col span={24}>
           <CopyrightWrapper>
             © {new Date().getFullYear()}, {` `}
--- src/components/header.js
+++ src/components/header.js
@@ -316,15 +316,4 @@ const MenuData = [
       </Icon>
     ),
   },
-  {
-    title: "Contact",
-    to: "/contact",
-    css: "linear-gradient(135deg, #E3FDF5 0%, #FFE6FA 100%)",
-    icon: (
-      <Icon name="map-pin">
-        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
-        <circle cx={12} cy={10} r={3} />
-      </Icon>
-    ),
-  },
 ]
