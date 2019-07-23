9ccd824e482717393f2fe4596c3ff1da4d4651c3
NamikawaRyotaro <n.ryo0328@gmail.com>
2019-07-23 12:58:55 +0900

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,11 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
+  graphql(
     `
       {
         posts: allMarkdownRemark(
-          filter: { fileAbsolutePath: { regex: "/blog|events|announcements/" } }
+          filter: { fileAbsolutePath: { regex: "/blog|announcements/" } }
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -47,9 +47,53 @@ exports.createPages = ({ graphql, actions }) => {
         },
       })
     })
-
-    return null
   })
+
+  const eventsPost = path.resolve(`./src/templates/events-post.js`)
+  graphql(
+    `
+      {
+        posts: allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/events/" } }
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fileAbsolutePath
+              frontmatter {
+                title
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.posts.edges
+
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.frontmatter.slug,
+        component: eventsPost,
+        context: {
+          slug: post.node.frontmatter.slug,
+          previous,
+          next,
+        },
+      })
+    })
+  })
+  return null
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
